505eb7044c98dbf0bfcae1e277ad0e6d3c8dfc80
John Underwood <4124456+JohnGUnderwood@users.noreply.github.com>
2024-03-22 22:24:50 +0000

    Revert "Tested local deployment on EC2 server"

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -68,8 +68,7 @@ export default function Home(){
 
 async function getFeeds() {
   return new Promise((resolve) => {
-    axios.get(`api/feeds`)
-      // axios.get(`${process.env.NEXT_PUBLIC_FEEDS_URL}:${process.env.NEXT_PUBLIC_FEEDS_PORT}/feeds`)
+      axios.get(`${process.env.NEXT_PUBLIC_FEEDS_URL}:${process.env.NEXT_PUBLIC_FEEDS_PORT}/feeds`)
       .then(response => resolve(response))
       .catch((error) => {
           console.log(error)
@@ -80,8 +79,7 @@ async function getFeeds() {
 
 async function getInstantResults(query) {
   return new Promise((resolve) => {
-    // axios.get(`${process.env.NEXT_PUBLIC_FEEDS_URL}:${process.env.NEXT_PUBLIC_FEEDS_PORT}/feeds/search?q=${query}`)
-    axios.get(`api/feeds/search?q=${query}`)
+    axios.get(`${process.env.NEXT_PUBLIC_FEEDS_URL}:${process.env.NEXT_PUBLIC_FEEDS_PORT}/feeds/search?q=${query}`)
     .then(response => resolve(response))
     .catch((error) => {
         console.log(error)
