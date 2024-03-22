2ffd0b6100307e257558f591aae586518959f7c4
JohnGUnderwood <4124456+JohnGUnderwood@users.noreply.github.com>
2024-03-22 15:52:44 +0000

    Feed submission wired up

---
+++ frontend/components/feeds/Form.js
@@ -0,0 +1,96 @@
+import styles from "./form.module.css";
+import { Label } from "@leafygreen-ui/typography";
+import TextInput from '@leafygreen-ui/text-input';
+import Icon from "@leafygreen-ui/icon";
+
+export default function Form({formData,setFormData}){
+    const handleInputChange = (attribute, event, index=null) => {
+        
+        if(attribute === 'content_html_selectors'){
+            setFormData({
+                ...formData, 
+                content_html_selectors: formData.content_html_selectors.map(
+                    (value, i) => i === index ? event.target.value : value)
+            });
+        }else {
+            setFormData({
+                ...formData,
+                [attribute]: event.target.value
+            });
+        }
+            
+    };
+
+    const handleAddClick = () => {
+        setFormData({
+            ...formData, 
+            content_html_selectors: formData.content_html_selectors.concat([''])
+        });
+    };
+
+    const handleRemoveClick = () => {
+        setFormData({
+            ...formData, 
+            content_html_selectors: formData.content_html_selectors.slice(0, formData.content_html_selectors.length - 1)
+        });
+    };
+
+    return (
+        <>
+        <div className={styles.formRow}>
+                <Label htmlFor="_id">ID:</Label>
+                <TextInput
+                    className={styles.formInput}
+                    type="text" id="_id" name="_id" required
+                    onChange={event => handleInputChange('_id',event)}/>
+                <div className={styles.spacer}></div>
+            </div>
+            <div className={styles.formRow}>
+                <Label htmlFor="lang">Language:</Label>
+                <TextInput
+                    className={styles.formInput}
+                    type="text" id="lang" name="lang" required
+                    onChange={event => handleInputChange('lang',event)}/>
+                <div className={styles.spacer}></div>
+            </div>
+            <div className={styles.formRow}>
+                <Label htmlFor="url">URL:</Label>
+                <TextInput
+                    className={styles.formInput}
+                    type="text" id="url" name="url" required
+                    onChange={event => handleInputChange('url',event)}/>
+                <div className={styles.spacer}></div>
+            </div>
+            <div className={styles.formRow}>
+                <Label htmlFor="attribution">Attribution:</Label>
+                <TextInput
+                    className={styles.formInput}
+                    type="text" id="attribution" name="attribution" required
+                    onChange={event => handleInputChange('attribution',event)}/>
+                <div className={styles.spacer}></div>
+            </div>
+            {formData.content_html_selectors.map((selector, index) => (
+                <div className={styles.formRow} key={index}>
+                    <Label htmlFor={`content_html_selector_${index}`}>Content HTML Selector:</Label>
+                    <TextInput
+                        className={styles.formInput}
+                        type="text"
+                        id={`content_html_selector_${index}`}
+                        name={`content_html_selector_${index}`}
+                        value={selector}
+                        onChange={event => handleInputChange('content_html_selectors',event,index)}
+                        required
+                    />
+                    {index === formData.content_html_selectors.length - 1 ? (
+                        <div className={styles.iconContainer}>
+                            <Icon onClick={handleAddClick} glyph={"PlusWithCircle"} fill="#C1C7C6" />
+                            {formData.content_html_selectors.length > 1 ? (
+                                <Icon onClick={handleRemoveClick} glyph={"XWithCircle"} fill="#C1C7C6" />
+                            ):<div style={{width:"16px"}}></div>}
+                        </div>
+                    ):<div className={styles.spacer}></div>}
+                </div>
+            ))}
+        </>
+    )
+}
